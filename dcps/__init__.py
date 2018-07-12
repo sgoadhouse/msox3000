e5bf16686b27a1286b21b6c4a27998004cbe6c48
Stephen Goadhouse <sgoadhouse@virginia.edu>
2018-07-12 19:49:02 -0400

    fixed __init__.py to work for python3 - still works in python2

--- dcps/__init__.py
+++ dcps/__init__.py
@@ -1,9 +1,9 @@
 
 # Standard SCPI commands
-from SCPI import SCPI
+from dcps.SCPI import SCPI
 
 # Support of Rigol DP832A and other DP800 power supplies
-from RigolDP800 import RigolDP800
+from dcps.RigolDP800 import RigolDP800
 
 # Support of Aim TTi PL-P Series power supplies
-from AimTTiPLP import AimTTiPLP
+from dcps.AimTTiPLP import AimTTiPLP

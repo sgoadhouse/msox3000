759000ec0f2d87390fe43a9339bc7e7cdbf8676f
Stephen Goadhouse <sgoadhouse@virginia.edu>
2018-07-12 20:05:52 -0400

    Updated version to 0.4.1 after minor change to __init__.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except(IOError, ImportError):
 
 
 setup(name="dcps", 
-      version='0.4',
+      version='0.4.1',
       description='Control of DC Power Supplies through python',
       long_description=long_description,
       url='https://github.com/sgoadhouse/dcps',
